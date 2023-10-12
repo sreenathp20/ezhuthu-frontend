8f21b80d1054d26026ac8f0b0442d3157ecd4901
sreenath puthiyaveettil <sreenath@sreenaths-MacBook-Pro.local>
2023-10-12 22:04:39 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import {environment} from './environments/environments'
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  if (environment.production) {
+    //enableProdMode();
+  }
+  throw new Error('White screen, reveal yourself!');
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
